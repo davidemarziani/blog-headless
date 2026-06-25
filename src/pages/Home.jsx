0217830a6b782c62bdd08e1bfd1b855004a89a29
davidemarziani <davide.marziani@gmail.com>
2026-06-25 15:33:10 +0200

    Aggiunto ! in homepage in Blog Headless

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ function Home() {
             <div className="hero-content">
                 <p className="eyebrow">WordPress Headless + React</p>
 
-                <h1 className="page-title">Blog Headless!</h1>
+                <h1 className="page-title">Blog Headless</h1>
 
                 <p className="page-text">
                     Questo frontend React leggerà gli articoli reali dal sito WordPress
